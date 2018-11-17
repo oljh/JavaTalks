6c1bf9fe148b2ae829b807d302e7de9578215815
oljh <34842956+oljh@users.noreply.github.com>
2018-11-17 17:07:11 +0300

    string

---
+++ src/ru/javatalks/stringReader/LineScanner.java
@@ -0,0 +1,15 @@
+package ru.javatalks.stringReader;
+
+import java.util.Scanner;
+
+public class LineScanner {
+	public static void main(String[] args) {
+		int n = 0;
+do {
+			System.out.println("Input how many string: ");
+			Scanner sc = new Scanner(System.in).useDelimiter("\\s");
+			n = sc.nextInt();
+} while (n < 1);
+
+	}
+}
