f4dd4ab590a1d09da6bd8c1f9ab483b8e5f2a07c
sapp <sapp@192.168.21.191>
2018-12-18 16:35:06 +0300

    otr

--- src/ru/javatalks/Arrays/otr/Otr.java
+++ src/ru/javatalks/Arrays/otr/Otr.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class Otr {
 
 	public static void main(String[] args) {
-		Integer[] A = null;
-		Integer[] B = null;
+		int[] A = null;
+		int[] B = null;
 		int sum = 0;
 		Random random = new Random();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Введите X размерность массива:");
 		int x = Integer.parseInt(sc.nextLine());
 		if (x > 0) {
-			A = new Integer[x];
+			A = new int[x];
 			System.out.println("Исходный массив:");
 			// Заполнение массива
 			for (int i = 0; i < x; i++) {
@@ -25,19 +25,23 @@ public class Otr {
 			System.out.println();
 
 			B = A.clone();
-
+			int k = 0;
 			// Проход по массиву A
 			for (int i = 0; i < x; i++) {
+				
+				// Проход по массиву B	
 				for (int j = 0; j < x; j++) {
 					if(i==j)continue;
 					if (A[i]<0 & A[i]==B[j]) {
-						sum+=A[i];
+					k++;
+						sum+=B[j];
 					}
 				}
 
 			}
 
 			System.out.println("Сумма отрицательных повторяющихся значений: " + sum);
+			System.out.println("Количество отрицательных повторяющихся значений: " + k);
 		}
 	}
 }
